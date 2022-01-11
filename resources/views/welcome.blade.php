cd90054c66a11ae71ff8770b1bec65c206ad275b
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2022-01-11 16:54:36 +0100

    TEST - Add Style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,13 +2,33 @@
 
 @section('content')
 
-<div class="container">
+<div class="container d_flex ">
 
     @foreach ($movies as $movie)
 
-    <div class="movieName">
-        {{$movie->title}}
+    <div class="movieCard">
+
+        <div class="titles">
+            <h3>Titolo/Originale</h3>
+            {{$movie->title}} /{{$movie->original_title}}
+        </div>
+
+        <div class="nationality">
+            <h3>Nazionalità</h3>
+            {{$movie->nationality}}
+        </div>
+
+        <div class="date">
+            <h3>Data di Uscita</h3>
+            {{$movie->date}}
+        </div>
+
+        <div class="vote">
+            <h3>Voto</h3>
+            {{$movie->vote}}
+        </div>
     </div>
+    <hr>
     @endforeach
 
 </div>
